2ffde611a6412c3a44c9ee6e98cb344113cc349f
loris <loris_labarre@outlook.fr>
2024-03-06 17:49:11 +0100

    Test pipeline with dangerous code

--- lib.js
+++ lib.js
@@ -1,3 +1,8 @@
 export function add(n1, n2){
     return n1+n2;
 }
+
+function displayMessage(userInput) {
+    var message = "Hello, " + userInput + "! Welcome to our website.";
+    document.getElementById("welcome-message").innerHTML = message;
+}
